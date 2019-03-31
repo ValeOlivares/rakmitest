60ba75261cda740cf48dc401d120bffbc71b01de
Valentina Neira <valentinaolivares85@gmail.com>
2019-03-31 20:54:08 -0300

    change passwords of omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '989732062212-mt78p86h245vedpi3bpmdtbtmafd6fq5.apps.googleusercontent.com', 'c-hA-ygW-DuzWstBJV3EKFgE', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '99704747938-j8bbd7ql51pg46jh5r8qjnqpom0ou2la.apps.googleusercontent.com', 'VE27CQC968i3gfm__k93lIbN', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
